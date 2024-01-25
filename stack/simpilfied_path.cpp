5e64f1438ef910485a187abd18fac0c0fac0a7e4
Varad <dvarad2607@gmail.com>
2024-01-25 12:24:38 -0800

    simplified path

--- stack/simpilfied_path.cpp
+++ stack/simpilfied_path.cpp
@@ -1,35 +1,53 @@
 #include <iostream>
 #include <string>
 #include <stack>
-#include <vector>
-#include <sstream>
 using namespace std;
 
-string simplified_path(string s) {
-    stack<string> stk;
-    stringstream ss(s);
-    string item;
-    while (getline(ss, item, '/')) {
-        if (item == "" || item == ".")
-            continue;
-        else if (item == ".." && !stk.empty())
-            stk.pop();
-        else if (item != "..")
-            stk.push(item);
-    }
-
+string simplified_path(string s){
+    stack<string>stk;
     string res;
-    while (!stk.empty()) {
-        res = "/" + stk.top() + res;
-        stk.pop();
-    }
+    string temp;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='/'){
+            continue;
+        }
+        temp="";
+        while(s[i]!='/' && i<s.length()){
+            temp+=s[i];
+            i++;
+        }
+        if(temp=="."){
+            continue;
+        }
+        else if(temp==".."){
+            if(!stk.empty()){
+                stk.pop();
+            }
+        }
+        else {
+            stk.push(temp);
+        }
+        
+       
 
-    return res.empty() ? "/" : res;
+    }  
+    while(!stk.empty()){
+            res="/"+stk.top()+res;
+            stk.pop();
+        }
+        if(res.size()==0){
+            return "/";
+        }
+    return res;   
+        
+        
+
 }
 
-int main() {
+int main(){
     string s;
-    getline(cin, s);
-    cout << "Simplified Path: " << simplified_path(s) << endl;
+    getline(cin,s);
+    cout << simplified_path(s) << endl;
     return 0;
+
 }
