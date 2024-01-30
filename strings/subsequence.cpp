0849eaa00d3c07fe5217eda989660b0e4e850621
Varad <dvarad2607@gmail.com>
2024-01-29 23:12:21 -0800

    dsa

---
+++ strings/subsequence.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool is_subsequence(string s_1,string s_2){
+    for(int i=0;i<s_1.length();i++){
+        if(s_2[i])
+
+    }
+
+}
+
+int main(){
+    string s_1;
+    getline(cin,s_1);
+    string s_2;
+    getline(cin,s_2);
+
+}
