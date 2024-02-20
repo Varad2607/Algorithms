b453275178cea87e58e0d681bba8cca7464ff0b1
Varad2607 <dvarad2607@gmail.com>
2024-02-20 00:34:50 -0800

    python basics

--- Arrays/pointers/test.cpp
+++ Arrays/pointers/test.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
 using namespace std;
 
-void arr (int *n){
-    cout<<*n<<endl;
-    *n=5;
-    cout<<*n<<endl;
+
+//If I want to send values to a function and change them, 
+// I can use pointers if using void function and if using int function, I can return the value and store it in a variable.
+// I can also use reference variables to change the value of a variable in a function.
+
+
+// void arr (int *n){
+//     cout<<*n<<endl;
+//     *n=5;
+//     cout<<*n<<endl;
+
+// }
+
+// int main(){
+//     int n=0;
+//     int *ptr = &n;
+//     arr(ptr);
+//     cout<<n<<endl;
+
+// }
+
+int arr (int n){
+    // cout<<n<<endl;
+    n=5;
+    // cout<<n<<endl;
+
+    return n;
 
 }
 
 int main(){
     int n=0;
-    int *ptr = &n;
-    arr(ptr);
+    int var=arr(n);
+    cout<<var<<endl;
     cout<<n<<endl;
 
 }
+
+
+
---
+++ Arrays/pointers/test.py
@@ -0,0 +1,58 @@
+#This is implemtation of try and except block in python
+def main():
+    try:
+        a=int(input("Enter a number: "))
+        print(a)
+    except ValueError:
+        print("Enter a valid number")
+
+#This is information about strings
+#remember strings have eol character at the end
+    name="Hello"
+    for i in range(len(name)):
+        print(name[i])
+        print(len(name))
+        print(name[5])
+    
+#String slicing
+    print(name[0:3]) #3 is not included
+    print(name[1:4]) #4 is not included (last index is not included)
+    print(name[1:]) #print from 1 to end
+    print(name[:4]) #print from 0 to 3
+    print(name[:]) #print whole string
+    print(name[-1]) #print last character
+    print(name[0:-2]) # this prints from 0 to 3
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+if __name__=="__main__":
+    main()
+    
