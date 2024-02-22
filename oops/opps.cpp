cf7f7718e56c1c43a4676b534b6493f79f2a3791
Varad2607 <dvarad2607@gmail.com>
2024-02-22 09:53:38 -0800

    oops1

--- oops/opps.cpp
+++ oops/opps.cpp
@@ -9,16 +9,28 @@ class student{
     int age;
     bool gender;
 
+    void printInfo(){
+        cout<<"Name: "<<name<<endl;
+        cout<<"Age: "<<age<<endl;
+    }
+
 };
 
 
 
 int main(){
-    student a;
-    a.name = "Rahul";
-    a.age =20;
-    a.gender=1;
+    student arr[3];
+    for(int i=0;i<3;i++){
+        cin>>arr[i].name;
+        cin>>arr[i].age;
+        cin>>arr[i].gender;
+    }
 
+    for(int i=0;i<3;i++){
+        arr[i].printInfo();
+        
+
+    }
 
     return 0;
 }
