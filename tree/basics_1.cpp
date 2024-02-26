381b9fe6a2c4658050fadedf7ab808d1ae82d7e0
Varad2607 <dvarad2607@gmail.com>
2024-02-26 13:14:03 -0800

    added trees

--- tree/basics_1.cpp
+++ tree/basics_1.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
+class node{
+    public:
     int data;
-    struct Node* left;
-    struct Node* right;
+    node* left;
+    node* right;
 
-    Node(int val){
-        data=val;
-        left=NULL;
-        right=NULL;
-
+    //Constructor
+    node(int val){
+        this->data=val;
+        this->left=NULL;
+        this->right=NULL;
     }
-
-
 
 
 };
 
+node* build_tree(node* root){
+    cout<<"Enter the data: ";
+    int data;
+    cin>>data;
+    root=new node(data);
+    if(data==-1){
+        return NULL;
+    }
+    cout<<"Enter element to left of "<<data<<endl;
+    root->left=build_tree(root->left);
+    cout<<"Enter element to right of "<<data<<endl;
+    root->right=build_tree(root->right);
+    return root;
+
+
+
+void level_order_traversal()
+
+
+}
+
 int main(){
-    struct Node* root=new Node(1);
-    root ->left=
+    node* root=NULL;
+
+    //creating tree
+    root=build_tree(root);
 
 }
