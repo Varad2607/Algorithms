9603cc1f8038bf7ef3b6924ffdcb6e3d5c80b896
Varad2607 <dvarad2607@gmail.com>
2024-10-26 12:44:38 -0700

    Shared_pointers

--- Arrays/pointerss.cpp
+++ Arrays/pointerss.cpp
@@ -6,10 +6,18 @@
 using namespace std;
 
 int main(){
+    //unique pointer
     unique_ptr<int> ptr1=make_unique<int>(10);
     unique_ptr<int> ptr2=move(ptr1);
     cout<<*ptr2<<endl;
+
+
+    //Shared Pointer
+    shared_ptr<int> ptr_shared=make_shared<int>(20);
+    shared_ptr<int> ptr_shared2=ptr_shared;
+    cout<<*ptr_shared<<endl;
+    cout<<*ptr_shared2<<endl;
     return 0;
 
-    
+
 }
