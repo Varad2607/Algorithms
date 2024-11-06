1b564fd8c0d59ac31a19c93cc7bb770c3eb3b35a
Varad2607 <dvarad2607@gmail.com>
2024-11-06 12:06:34 -0800

    hashmap2

--- hashmap/isomorphic.py
+++ hashmap/isomorphic.py
@@ -1,28 +1,19 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        dict_1={}
-        dict_2={}
-        for char in s:
-            if char not in dict_1:
-                dict_1[char]=1
-            else:
-                dict_1[char]+=1
-        print(dict_1)
-        for char_1 in t:
-            if char_1 not in dict_2:
-                dict_2[char_1]=1
-            else:
-                dict_2[char_1]+=1
-        print(dict_2)
-        for chars in s:
-            if dict_1[char]!=dict_2[char_1]:
+        map_s,map_t={},{}
+        for char_s,char_t in zip(s,t):
+            if ((char_s in map_s and map_s[char_s]!=char_t) or
+                (char_t in map_t and map_t[char_t]!=char_s)):
                 return False
-        return True
 
+            map_s[char_s]=char_t
+            map_t[char_t]=char_s
+        return True
+        
 
 
 if __name__=="__main__":
     s=Solution()
-    print(s.isIsomorphic("egg","add"))
+    print(s.isIsomorphic("badc","baba"))
 
         
