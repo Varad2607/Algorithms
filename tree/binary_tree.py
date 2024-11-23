bf5159fc0a6f944725f2c67242c15ae72e7806c0
Varad2607 <dvarad2607@gmail.com>
2024-11-22 19:51:22 -0800

    day2

--- tree/binary_tree.py
+++ tree/binary_tree.py
@@ -1,5 +1,7 @@
 import queue
 
+
+
 class Node:
     def __init__(self,value):
         self.value=value
@@ -67,6 +69,50 @@ class Node:
                     q.add(current_node.left)
                 if current_node.right!=None:
                     q.add(current_node.right)
+
+    def count_nodes(self):
+        if self.value==None:
+            return None
+
+        countleft=self.count_nodes(self.left)
+        countright=self.count_nodes(self.right)
+        return countleft +countright +1
+
+    def sum_of_nodes(self):
+        if self.value==None:
+            return None
+
+        sumleft=self.sum_of_nodes(self.left)
+        sumright=self.sum_of_nodes(self.right)
+
+        return sumleft + sumright + self.value
+
+    def height_of_tree(self):
+        if self.value==None:
+            return None
+
+        height_left=self.height_of_tree(self.left)
+        height_right=self.height_of_tree(self.right)
+        max_height=max(height_left,height_right)
+
+        return 1 + max_height
+
+
+    def diamter_of_tree(self):
+        if self is None:
+            return 0
+
+        diameter_left=self.left.diameter_left()
+        diameter_right=self.right.diamter_right()
+        diamter_left_right=self.left.height_of_tree()+self.right.height_of_tree()
+
+        return max(diameter_left,max(diameter_left,diameter_right))
+
+
+
+
+
+        pass
 
 
 
