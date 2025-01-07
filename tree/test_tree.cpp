85fc63b4d6946b2725a18b9c76943eeb5c2c0b9f
Varad2607 <dvarad2607@gmail.com>
2025-01-07 00:12:06 -0800

    level order traversal

--- tree/test_tree.cpp
+++ tree/test_tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 
@@ -55,6 +56,54 @@ void postorder_traversal(Node *root){
 }
 
 
+//Level order traversal
+
+void levelorder_traversal(Node *root){
+    // Add the root of the tree in queue data structure
+    // use a while loop and that loop will go until the queue is empty
+    // if it is Null found then give space and print it
+    // Add Null again
+    // if not null 
+    // add root.left
+    // add root.right
+    queue<Node*>q;
+    q.push(root);
+    q.push(nullptr);
+
+    while (!q.empty()){
+        Node* current_node=q.front();
+        q.pop();
+        if(current_node==nullptr){
+            cout<<endl;
+            if(!q.empty()){
+                q.push(nullptr);
+            }
+            
+        }
+        else{
+            cout<<current_node->data<<" ";
+
+            if (current_node->left!=nullptr){
+                q.push(current_node->left);
+            }
+            if (current_node->right!=nullptr){
+                q.push(current_node->right)l;
+            }
+
+
+                
+            }
+
+
+    }
+
+
+
+
+       
+
+}
+
 int main(){
     Node *root= new Node(1);
     root->left=new Node(2);
@@ -62,5 +111,6 @@ int main(){
     inorder_traversal(root);
     preorder_traversal(root);
     postorder_traversal(root);
+    levelorder_traversal(root);
     
 }
